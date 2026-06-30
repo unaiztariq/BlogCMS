2c95bb5becd64b4e7caa7594c6b1011799e8299e
unaiztariq <unaiztariq@gmail.com>
2026-06-30 03:15:02 -0700

    Update settings.py

--- BlogCMS/settings.py
+++ BlogCMS/settings.py
@@ -138,7 +138,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = "unaiztariqkhan@gmail.com"
-EMAIL_HOST_PASSWORD = "jotm havg xxqj imcp"
+EMAIL_HOST_PASSWORD = ""
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
